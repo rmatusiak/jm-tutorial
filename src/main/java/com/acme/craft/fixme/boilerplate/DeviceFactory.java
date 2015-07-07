a1321b8129b5b56d4722a2488184f4847b94c89e
rmatusiak <radoslaw.matusiak@stazysta.comarch.pl>
2015-07-07 14:27:04 +0200

    Add class DeviceFactory

---
+++ src/main/java/com/acme/craft/fixme/boilerplate/DeviceFactory.java
@@ -0,0 +1,17 @@
+package com.acme.craft.fixme.boilerplate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class DeviceFactory {
+	
+	private String idid;
+	
+	public void create (DeviceDto deviceDto){
+		deviceDto.getName();
+	}
+
+}
